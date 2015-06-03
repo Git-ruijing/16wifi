673bf07e485e6908c20a0011061d119e7cc205b9
Ladydudu <115413@163.com>
2015-06-03 11:32:30 +0800

    netChange

--- e-RoadWiFi/MainViewController/NetPlayViewController.m
+++ e-RoadWiFi/MainViewController/NetPlayViewController.m
@@ -24,9 +24,7 @@
 #define FREE114IN @"http://wifi.gd118114.cn/login.ajax"
 #define FREE114OUT @"http://wifi.gd118114.cn/logout.ajax"
 @interface NetPlayViewController ()
-//sss
 @end
-//解决冲突域
 @implementation NetPlayViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
